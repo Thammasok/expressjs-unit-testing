37c89e58283358e01d8387d16758538c82771703
Jaranchai <jaranchai.nt@hotmail.com>
2018-09-12 17:49:40 +0700

    change text on console log

--- test/basic.test.js
+++ test/basic.test.js
@@ -1,28 +1,33 @@
 // const assert = require('assert');
 
-// describe('คำอธิบายว่าเราจะ Test เรื่องอะไร', () => {
-//   beforeEach(() => {
-//     // beforeEach ทุก Test case เราจะเข้า function นี้ก่อนเสมอ
-//     console.log('beforeEach')
-//   });
+describe('แสดงลำดับการทำงานของ Function ต่างๆ ใน Mocha', () => {
+  beforeEach(() => {
+    // beforeEach ทุก Test case เราจะเข้า function นี้ก่อนเสมอ
+    console.log('2. beforeEach: ทุก Test case เราจะเข้า function นี้ก่อนเสมอ')
+  });
   
-//   before(() => {
-//     // before คือ เราจะทำ function นี่ก่อนทำ Test case ครั้งแรกครั้งเดียว
-//     console.log('before')
-//   });
+  before(() => {
+    // before คือ เราจะทำ function นี่ก่อนทำ Test case ครั้งแรกครั้งเดียว
+    console.log('1. before: จะทำ function นี่ก่อนทำ Test case ครั้งแรกครั้งเดียว')
+  });
 
-//   afterEach(() => {
-//     // afterEach เมื่อจบ Test case แต่ละ Test case เราจะทำ function นี้
-//     console.log('afterEach')
-//   });
+  afterEach(() => {
+    // afterEach เมื่อจบ Test case แต่ละ Test case เราจะทำ function นี้
+    console.log('4. afterEach: เมื่อจบ Test case แต่ละ Test case เราจะทำ function นี้')
+  });
 
-//   after(() => {
-//     // after เมื่อจบทุก Test case ถึงจะทำ function นี้
-//     console.log('after')
-//   });
+  after(() => {
+    // after เมื่อจบทุก Test case ถึงจะทำ function นี้
+    console.log('6. after: เมื่อจบทุก Test case ถึงจะทำ function นี้')
+  });
 
-//   it('คำอธิบาย Test case', () => {
-//     // test case
-//     console.log('it')
-//   });
-// });
+  it('Test case: 1', () => {
+    // test case
+    console.log('3. it 1')
+  });
+
+  it('Test case: 2', () => {
+    // test case
+    console.log('5. it 2')
+  });
+});
